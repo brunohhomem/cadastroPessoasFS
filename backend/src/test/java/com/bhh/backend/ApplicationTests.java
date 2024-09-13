e88fb56beba52fa964a222eddd45e135db5a1e4c
Bruno H. Homem <brunohhomem@gmail.com>
2024-09-13 18:31:33 -0300

    chore: added database, docker and environment configs

--- backend/src/test/java/com/bhh/backend/ApplicationTests.java
+++ backend/src/test/java/com/bhh/backend/ApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class CadastroPessoasApplicationTests {
+class ApplicationTests {
 
 	@Test
 	void contextLoads() {
