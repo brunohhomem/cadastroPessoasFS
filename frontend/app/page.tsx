8ea7d753f1edf48435fc4513548864a4a3d4d53c
Bruno H. Homem <brunohhomem@gmail.com>
2024-09-16 19:21:29 -0300

    feat: added `pessoa-card` static

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,7 +1,11 @@
-// import PessoaCard from '@/components/pessoa-card'
-import PessoaList from '@/components/pessoa-list'
+import PessoaCard from '@/components/pessoa-card'
+// import PessoaList from '@/components/pessoa-list'
 
 export default function Home() {
-  return <PessoaList />
-  // return <PessoaCard />
+  // return <PessoaList />
+  return (
+    <div className="m-5">
+      <PessoaCard />
+    </div>
+  )
 }
--- frontend/components/pessoa-card.tsx
+++ frontend/components/pessoa-card.tsx
@@ -7,21 +7,46 @@ import {
   CardTitle
 } from './ui/card'
 
+import {
+  BadgeCheck,
+  CalendarFold,
+  Mail,
+  Phone,
+  UserRoundPen,
+  UserX
+} from 'lucide-react'
+
 export default function PessoaCard() {
   return (
-    <Card>
+    <Card className="w-72">
       <CardHeader>
-        <CardTitle>Bruno</CardTitle>
-        <CardDescription>Descrição do Bruno</CardDescription>
+        <CardTitle>Bruno Homem</CardTitle>
+        <CardDescription className="flex gap-2">
+          Ativo
+          <BadgeCheck />
+        </CardDescription>
       </CardHeader>
       <CardContent>
-        <p>Card Content</p>
-      </CardContent>
-      <CardContent>
-        <p>Card Content</p>
+        <div className="flex gap-2">
+          <Mail />
+          <a className="underline" href="mailto:brunohhomem@email.com">
+            brunohhomem@email.com
+          </a>
+        </div>
+        <div className="flex gap-2">
+          <Phone />
+          <p>(11) 9988771166</p>
+        </div>
+        <div className="flex gap-2">
+          <CalendarFold />
+          <p>25/12/2024</p>
+        </div>
       </CardContent>
       <CardFooter>
-        <p>Card Footer</p>
+        <div className="flex gap-2">
+          <UserX />
+          <UserRoundPen />
+        </div>
       </CardFooter>
     </Card>
   )
