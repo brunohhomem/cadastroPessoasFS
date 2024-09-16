716a135e2ede767b6398add5d14904bd99e17789
Bruno H. Homem <brunohhomem@gmail.com>
2024-09-16 19:47:05 -0300

    feat: `pessoa-card` with api data

--- frontend/components/pessoa-card.tsx
+++ frontend/components/pessoa-card.tsx
@@ -1,3 +1,6 @@
+'use client'
+
+import { PessoaDisplayDTO } from '@/types/pessoa'
 import {
   Card,
   CardContent,
@@ -8,46 +11,73 @@ import {
 } from './ui/card'
 
 import {
-  BadgeCheck,
   CalendarFold,
+  CircleCheck,
+  CircleX,
   Mail,
   Phone,
   UserRoundPen,
   UserX
 } from 'lucide-react'
+import { useEffect, useState } from 'react'
+import { listarPessoas } from '@/services/pessoa-service'
 
 export default function PessoaCard() {
+  const [pessoas, setPessoas] = useState<PessoaDisplayDTO[]>([])
+
+  useEffect(() => {
+    const fetchPessoas = async () => {
+      const data = await listarPessoas()
+      setPessoas(data)
+    }
+
+    fetchPessoas()
+  }, [])
+
   return (
-    <Card className="w-72">
-      <CardHeader>
-        <CardTitle>Bruno Homem</CardTitle>
-        <CardDescription className="flex gap-2">
-          Ativo
-          <BadgeCheck />
-        </CardDescription>
-      </CardHeader>
-      <CardContent>
-        <div className="flex gap-2">
-          <Mail />
-          <a className="underline" href="mailto:brunohhomem@email.com">
-            brunohhomem@email.com
-          </a>
-        </div>
-        <div className="flex gap-2">
-          <Phone />
-          <p>(11) 9988771166</p>
-        </div>
-        <div className="flex gap-2">
-          <CalendarFold />
-          <p>25/12/2024</p>
-        </div>
-      </CardContent>
-      <CardFooter>
-        <div className="flex gap-2">
-          <UserX />
-          <UserRoundPen />
-        </div>
-      </CardFooter>
-    </Card>
+    <div className="flex">
+      {pessoas.map(pessoa => (
+        <Card className="w-72 m-2" key={pessoa.id}>
+          <CardHeader>
+            <CardTitle>
+              {pessoa.nome} {pessoa.sobrenome}
+            </CardTitle>
+            <CardDescription className="flex gap-1">
+              {pessoa.isActive ? (
+                <p className="flex gap-1">
+                  Ativo <CircleCheck className="text-green-500" />
+                </p>
+              ) : (
+                <p className="flex gap-1">
+                  Inativo <CircleX className="text-red-500" />
+                </p>
+              )}
+            </CardDescription>
+          </CardHeader>
+          <CardContent>
+            <div className="flex gap-2">
+              <Mail />
+              <a className="underline" href="mailto:{pessoa.fone}">
+                {pessoa.email}
+              </a>
+            </div>
+            <div className="flex gap-2">
+              <Phone />
+              <p>{pessoa.fone}</p>
+            </div>
+            <div className="flex gap-2">
+              <CalendarFold />
+              {new Date(pessoa.createdAt).toLocaleDateString()}
+            </div>
+          </CardContent>
+          <CardFooter>
+            <div className="flex gap-2">
+              <UserX />
+              <UserRoundPen />
+            </div>
+          </CardFooter>
+        </Card>
+      ))}
+    </div>
   )
 }
