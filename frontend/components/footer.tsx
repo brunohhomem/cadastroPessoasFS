fbffec1b1ff1e333b479f64c5aafcd7a04a48420
Bruno H. Homem <brunohhomem@gmail.com>
2024-09-19 18:50:16 -0300

    fix: margin footer

--- frontend/components/footer.tsx
+++ frontend/components/footer.tsx
@@ -1,6 +1,7 @@
 export default function Footer() {
   return (
-    <footer className="mb-10 mt-10 px-4 text-center text-gray-50 bg-slate-500 mt-64">
+    <footer className="mb-10 px-4 text-center text-gray-50 bg-slate-500 mt-64">
+      {/* <footer className="mb-10 mt-10 px-4 text-center text-gray-50 bg-slate-500 mt-64"> */}
       <p>
         <span>Criado com ❤️, utilizando: </span>
         <br />
