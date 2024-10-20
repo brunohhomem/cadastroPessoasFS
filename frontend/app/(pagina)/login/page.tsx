92193a6ac4e5fb9af438713b735e38b4cd24067a
Bruno H. Homem <brunohhomem@gmail.com>
2024-10-20 10:54:33 -0300

    feat: structure of login card

--- frontend/app/(pagina)/login/page.tsx
+++ frontend/app/(pagina)/login/page.tsx
@@ -1,3 +1,27 @@
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+
 export default function Login(){
-  return <h1>login page</h1>
+  return (
+    <main className="flex items-center justify-center min-h-screen">
+      <div className="bg-slate-500 p-2 rounded-lg shadow-lg">
+        <Card className="w-72 m-2">
+        <CardHeader>
+          <CardTitle>
+            Sistema de Cadastro de Pessoas
+          </CardTitle>
+          <CardDescription className="flex gap-1">
+            <h2>Efetue login para continuar</h2>
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+
+        </CardContent>
+        <CardFooter>
+          <div className="flex gap-2">
+          </div>
+        </CardFooter>
+      </Card>
+    </div>
+  </main>
+  )
 }
